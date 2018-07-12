d1ca378043c01cb745737264c58a82ad0035e5be
Hsinny <hsinnyliu@gmail.com>
2018-07-12 10:27:20 +0800

    主要功能完成：   1. 下拉式選單切換市區，顯示各區災情   2. 介接 Google Map API，顯示全部災情
    效能優化：
      1. 頁碼表完成，每頁顯示預設筆數

--- js/typhoon_api.js
+++ js/typhoon_api.js
@@ -1,64 +1,407 @@
 var zone = document.getElementById('select-zone');
-var detail = document.querySelector('.detail');
+var detail = document.querySelector('.detail');  // tbody
 var counter = document.getElementById('counter');
+var pagination = document.getElementById('pagination');
+var jsonData = {};
+var jsonDataLen = new Number;
 
-var zoneData = ["全區", "中正區", "大同區", "中山區", "松山區", "大安區", "萬華區", "信義區", "士林區", "北投區", "內湖區", "南港區", "文山區"];
-var zoneLen = zoneData.length;
-var zoneStr = '';
-    zoneData.sort();
-    for(let i=0; i<zoneLen; i++){
-      if(i==0){
-        zoneStr = '<option value="' + zoneData[i] + '">-請選擇行政區-</option>';
-      }
-      zoneStr += '<option value="' + zoneData[i] + '">' + zoneData[i] + '</option>';
-    }
-    zone.innerHTML = zoneStr;
+// 資料撈進來自動生成選單內的選項
+var selectItem = [];
+var selectItemLen = new Number;
 
+// 存條件符合的資料
+var data = [];
+
+// 分頁元件
+var prevBtn = `<li class="page-item">
+                  <a class="page-link icon-prev" href="#" aria-label="Previous" data-num="-1"></a>
+                </li>`;
+
+var nextBtn = `<li class="page-item">
+                  <a class="page-link icon-next" href="#" aria-label="Next" data-num="1"></a>
+                </li>`;
 
 // 向伺服器要資料、readyState=4後顯示在網頁上
-function getData(selectZone) {
-  var xhr = new XMLHttpRequest();
-  xhr.open('get', 'https://tpeoc.blob.core.windows.net/blobfs/GetDisasterSummary.json', true);
-  xhr.send(null);
-  
+var xhr = new XMLHttpRequest();
+xhr.open('get', 'https://next.json-generator.com/api/json/get/Ek86hdRMr', true);
+xhr.send(null);
+// https://tpeoc.blob.core.windows.net/blobfs/GetDisasterSummary.json
 
+
+callAjax();
+
+function callAjax(){
   xhr.onload = function () {
-    var jsonData = JSON.parse(xhr.responseText);
-    var jsonDataLen = jsonData.length;
-    var number = 0;
-    counter.textContent = jsonDataLen;
-    for (let i = 0; i < jsonDataLen; i++){
-      if (jsonData[i].CaseLocationDistrict == selectZone){
-        number++;
-        detail.innerHTML +=`
-        <tr>
-          <th>${number}</th>
-          <th>${jsonData[i].CaseTime}</th>
-          <th>${jsonData[i].CaseLocationDistrict}</th>
-          <th>${jsonData[i].CaseLocationDescription}</th>
-          <th>${jsonData[i].CaseDescription}</th>
-        </tr>`;
-      } else {
-        number++;
-        detail.innerHTML += `
-        <tr>
-          <th>${number}</th>
-          <th>${jsonData[i].CaseTime}</th>
-          <th>${jsonData[i].CaseLocationDistrict}</th>
-          <th>${jsonData[i].CaseLocationDescription}</th>
-          <th>${jsonData[i].CaseDescription}</th>
-        </tr>`;
-      }
+    jsonData = JSON.parse(xhr.responseText);
+    data = jsonData;
+    jsonDataLen = jsonData.length;
+    counter.textContent = jsonDataLen; // 資料總筆數
+
+    // 若載入的時候已經有產生選單之後就不再做
+    if (selectItemLen < 1) {
+      renderOption(jsonData);
+    }
+    // 渲染內容
+    renderContent(1); // 參數 => 第1頁
+  }
+}
+
+
+
+// 將撈到的資料，判斷有哪些地區，並建立成下拉選單的選項
+// 渲染下拉選單
+function renderOption(option) {
+  for (var i = 0; i < option.length; i++) {
+    if (selectItem.indexOf(option[i].CaseLocationDistrict) == -1 ) {
+      selectItem.push(option[i].CaseLocationDistrict);
+    }
+  }
+
+  
+  // 陣列內資料重新排序
+  selectItem.sort();  
+  var optionLen = selectItem.length;
+  var zoneStr = '';
+
+  // 把'其他'這個選項排序到最後
+  for (let i = 0; i < optionLen; i++) {
+    if (selectItem[i] === '其他') {
+      var index = i;
+      var option = selectItem[i];
+    }
+  }
+  selectItem.splice(index, 1);
+  selectItem.push(option);
+
+
+  // 將selectItem內的資料渲染到option內
+  for (let i = 0; i < optionLen; i++) {
+    if (i == 0) {
+      zoneStr = '<option value="' + selectItem[i] + '">-請選擇行政區-</option>';
+    }
+    zoneStr += '<option value="' + selectItem[i] + '">' + selectItem[i] + '</option>';
+  }
+  zone.innerHTML = zoneStr;
+}
+
+
+
+// 目前頁數、總頁數、總共幾筆、要前往的頁數
+var currentPage, totalPage, totalItem;
+// 一頁10筆資料
+var perPage = 10;
+
+function renderContent(goPage){
+  totalItem = data.length;
+  // 計算總共有幾頁(使用無條件進位)
+  totalPage = Math.ceil(totalItem / perPage);
+
+  // 起始資料index,結束資料index
+  var startItem;
+  var endItem;
+
+  // 如果是最後一頁要判斷抓取幾筆資料，其餘都一定是perPage指定的筆數
+  if (goPage == totalPage) {
+    var minusItem = totalItem - (totalPage * perPage);
+
+    if (minusItem == 0) { //判斷最後一頁是幾筆用 = 0 就是10筆
+      startItem = ((totalPage - 1) * perPage);
+      endItem = totalItem;
+    } else { // 小於10筆
+      startItem = ((totalPage - 1) * perPage);
+      endItem = totalItem;
+    }
+  } else {
+    startItem = perPage * (goPage - 1);
+    endItem = (goPage * 10);
+  }
+
+  // 資料渲染到畫面
+  detail.innerHTML = ''; // 清除先前顯示的資料
+  for(var i = startItem; i<endItem; i++){
+    startItem++;
+    detail.innerHTML += `<tr>
+      <th>${startItem}</th>
+      <th>${data[i].CaseTime}</th>
+      <th>${data[i].CaseLocationDistrict}</th>
+      <th>${data[i].CaseLocationDescription}</th>
+      <th>${data[i].CaseDescription}</th>
+    </tr>`;
+  }
+
+
+  // 紀錄目前頁數用來點選上下頁用
+  currentPage = goPage;
+  // 渲染頁碼
+  renderPage(totalPage);
+  initMap();
+}
+
+
+// 渲染有幾頁用
+function renderPage(totalPage){
+  
+  if (totalPage>0) {
+    pagination.innerHTML = '';
+    var pageBtn ='';
+    for(let i=0; i<totalPage; i++){
+      pageBtn += `<li class="mpage-ite"><a class="page-link" href="#" data-page="${(i + 1)}">${(i + 1)}</a></li>`;
+    }
+    pagination.innerHTML = prevBtn + pageBtn + nextBtn;
+  }
+}
+
+
+// 重新將查詢的資料放入到新的 array
+function queryArea(zoneName) {
+   // 清空 
+  data = [];
+  for (let i = 0; i < jsonDataLen; i++){
+    if (jsonData[i].CaseLocationDistrict == zoneName) {
+      data.push(jsonData[i]);
     }
   }
 }
 
 
-// selector 事件
-function filterData(){
-  var selectZone = zone.value;
-  detail.innerHTML = ''; // 清除先前顯示的資料
-  getData(selectZone);
-}
+// 監聽 select 變動
+zone.addEventListener('change', function(e){
+  e.preventDefault();
+  queryArea(e.target.value);
+  renderContent(1);
+}, false);
 
-zone.addEventListener('change', filterData,false);
+
+// 頁次偵聽
+pagination.addEventListener('click', function(e){
+  e.preventDefault();
+  var goPage;
+  var prevNext = Number(e.target.dataset.num);
+  console.log(e.target);
+  
+  // 當有按下下一頁或上頁
+  if (prevNext == -1 || prevNext == 1) {
+    if (prevNext == -1) {
+      if (currentPage + prevNext < 1) {
+        return false;
+      }
+      goPage = currentPage - 1;
+    } else if (prevNext == 1) {
+      if (currentPage + prevNext > totalPage) {
+        return false;
+      }
+      goPage = currentPage + 1;
+    }
+  } else {
+    goPage = Number(e.target.dataset.page); //Number() 把對象的值轉換為數字
+    if (currentPage == goPage){
+      return;
+    }
+  }
+  renderContent(goPage);
+},false);
+
+
+
+/*===================================================================*/
+/* Google Map
+/*===================================================================*/
+var map;
+var mapData = [];
+
+
+
+function initMap() {
+  // 選取地圖DOM & center 中心點 & zoom 縮放層級 & styles 地圖樣式
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: { lat: 25.0479, lng: 121.555 },
+    zoom: 13,
+    styles: [
+      {
+        "featureType": "landscape.natural",
+        "stylers": [
+          {
+            "color": "#bcddff"
+          }
+        ]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "geometry.fill",
+        "stylers": [
+          {
+            "color": "#5fb3ff"
+          }
+        ]
+      },
+      {
+        "featureType": "road.arterial",
+        "stylers": [
+          {
+            "color": "#ebf4ff"
+          }
+        ]
+      },
+      {
+        "featureType": "road.local",
+        "elementType": "geometry.fill",
+        "stylers": [
+          {
+            "color": "#ebf4ff"
+          }
+        ]
+      },
+      {
+        "featureType": "road.local",
+        "elementType": "geometry.stroke",
+        "stylers": [
+          {
+            "visibility": "on"
+          },
+          {
+            "color": "#93c8ff"
+          }
+        ]
+      },
+      {
+        "featureType": "landscape.man_made",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#c7e2ff"
+          }
+        ]
+      },
+      {
+        "featureType": "transit.station.airport",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "saturation": 100
+          },
+          {
+            "gamma": 0.82
+          },
+          {
+            "hue": "#0088ff"
+          }
+        ]
+      },
+      {
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#1673cb"
+          }
+        ]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "labels.icon",
+        "stylers": [
+          {
+            "saturation": 58
+          },
+          {
+            "hue": "#006eff"
+          }
+        ]
+      },
+      {
+        "featureType": "poi",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#4797e0"
+          }
+        ]
+      },
+      {
+        "featureType": "poi.park",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#209ee1"
+          },
+          {
+            "lightness": 49
+          }
+        ]
+      },
+      {
+        "featureType": "transit.line",
+        "elementType": "geometry.fill",
+        "stylers": [
+          {
+            "color": "#83befc"
+          }
+        ]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "geometry.stroke",
+        "stylers": [
+          {
+            "color": "#3ea3ff"
+          }
+        ]
+      },
+      {
+        "featureType": "administrative",
+        "elementType": "geometry.stroke",
+        "stylers": [
+          {
+            "saturation": 86
+          },
+          {
+            "hue": "#0077ff"
+          },
+          {
+            "weight": 0.8
+          }
+        ]
+      },
+      {
+        "elementType": "labels.icon",
+        "stylers": [
+          {
+            "hue": "#0066ff"
+          },
+          {
+            "weight": 1.9
+          }
+        ]
+      },
+      {
+        "featureType": "poi",
+        "elementType": "geometry.fill",
+        "stylers": [
+          {
+            "hue": "#0077ff"
+          },
+          {
+            "saturation": -7
+          },
+          {
+            "lightness": 24
+          }
+        ]
+      }
+    ]
+  });
+
+
+  // 標記顯示
+  var marker;
+  for (let i = 0; i < totalItem; i++){
+    var markerObj = { 
+      position: { lat: data[i].Wgs84Y, lng: data[i].Wgs84X},
+      map: map, // 要執行在哪張地圖上
+      // label: data[i].CaseDescription,
+      icon: 'https://imgur.com/WLSXfKQ.png'
+      // title: data[i].CaseDescription
+    };
+    mapData.push(markerObj);
+    marker = new google.maps.Marker(markerObj);
+  }
+}
